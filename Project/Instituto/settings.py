f4930b5835b7655d6b5801bcf433000817e30550
Di-Mella <di.mella@duocuc.cl>
2024-06-18 03:00:34 -0400

    CRUD

--- Project/Instituto/settings.py
+++ Project/Instituto/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-wk%nko4rv7_9xmryr+2&25l@ay&r^f64hevjdm8x06g)i@1iw&'
+print(BASE_DIR)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+STATIC_URL = '/static/'
 
 # Application definition
 
@@ -117,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- Project/alumnos/views.py
+++ Project/alumnos/views.py
@@ -6,4 +6,116 @@ def index(request):
     context ={'alumnos':alumnos}
     return render(request, 'alumnos/index.html', context)
 
+def crud(request):
+    alumnos = Alumno.objects.all()
+    context = {'alumnos':alumnos}
+    return render(request, 'alumnos/alumnos_list.html',context)
+
+def alumnosAdd(request):
+    if request.method is not "POST":
+        #NO ES UN POST, MOSTRARA EL FORMULARIO A AGREGAR.
+        generos=Genero.objects.all()
+        context={'generos':generos}
+        return render(request, 'alumnos/alumnos_add.html', context)
+
+    else:
+        # Es un post, por lo tanto se recyuperan los datos del formulario y se graban en la tabla
+        rut=request.POST["rut"]
+        nombre=request.POST["nombre"]
+        aPaterno=request.POST["paterno"]
+        aMaterno=request.POST["materno"]
+        fechaNac=request.POST["fechaNac"]
+        genero=request.POST["genero"]
+        telefono =request.POST["telefono"]
+        email =request.POST["email"]
+        direccion=request.POST["direccion"]
+        activo="1"
+
+        objGenero=Genero.objects.get(id_genero = genero)
+        obj=Alumno.objects.create(  rut = rut,
+                                    nombre = nombre,
+                                    apellido_paterno = aPaterno, 
+                                    apellido_materno = aMaterno,
+                                    fecha_nacimiento = fechaNac,
+                                    id_genero = objGenero,
+                                    telefono = telefono,
+                                    email = email,
+                                    direccion = direccion,
+                                    activo = 1      
+        )
+        obj.save()
+        context={'mensaje': "Ok, datos grabados ..."}
+        return render(request,'alumnos/alumnos_add.html', context)
+    
+
+def alumnos_del(request,pk):
+    context={}
+    try: 
+            alumno=Alumno.objects.get(rut=pk)
+
+            alumno.delete()
+            mensaje="Bien, los datos han sido eliminados..."
+            alumnos= Alumno.objects.all()
+            context={'alumnos': alumnos, 'mensaje': mensaje}
+            return render(request, 'alumnos/alumnos_list.html',context)
+    except:
+            mensaje="Error, rut no existe..."
+            alumnos= Alumno.objects.all()
+            context= {'alumnos': alumnos, 'mensaje': mensaje}
+            return render(request, 'alumnos/alumnos_list.html',context)
+    
+def alumnos_findEdit (request,pk):
+    
+    if pk!= "":
+        alumno=Alumno.objects.get(rut=pk)
+        generos=Genero.objects.all()
+
+        print(type(alumno.id_genero.genero))
+
+        context={'alumno':alumno, 'generos':generos}
+        if alumno:
+            return render(request, 'alumnos/alumnos_edit.html',context)
+        else:
+            context={'mensaje':"Error, rut no existe..."}
+            return render(request, 'alumnos/alumnos_list.html',context)
+        
+def alumnosUpdate(request):
+     
+    if request.method == "POST" :   
+        # Es un post, por lo tanto se recyuperan los datos del formulario y se graban en la tabla
+        rut=request.POST["rut"]
+        nombre=request.POST["nombre"]
+        aPaterno=request.POST["paterno"]
+        aMaterno=request.POST["materno"]
+        fechaNac=request.POST["fechaNac"]
+        genero=request.POST["genero"]
+        telefono =request.POST["telefono"]
+        email =request.POST["email"]
+        direccion=request.POST["direccion"]
+        activo="1"
+
+        objGenero=Genero.objects.get(id_genero = genero)
+
+        alumno = Alumno()
+        alumno.rut = rut
+        alumno.nombre = nombre
+        alumno.apellido_paterno = aPaterno
+        alumno.apellido_materno = aMaterno
+        alumno.fecha_nacimiento = fechaNac
+        alumno.id_genero = objGenero
+        alumno.telefono = telefono
+        alumno.email = email
+        alumno.direccion = direccion
+        alumno.activo = 1
+        alumno.save()
+
+        generos=Genero.objects.all()
+        context={'mensaje': "Ok, datos actualizados...", 'generos':generos,'alumno':alumno }
+        return render(request, 'alumno/alumnos_edit.html',context)
+    else:
+        alumnos= Alumno.objects.all()
+        context={'alumnos':alumnos}
+        return render (request, 'alumnos/alumnos_list.html',context)
+
+
 
